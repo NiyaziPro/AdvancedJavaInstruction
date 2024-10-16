a3e48dfb791fcc29831493472bca74ec4baeddb2
NiyaziPro <nyzjunior@gmail.com>
2024-10-16 13:52:54 +0200

    13:52 Wednesday 16.10.2024

---
+++ src/threads/DeadLockSolution2.java
@@ -0,0 +1,60 @@
+package threads;
+/*
+DeadLock: occurs when two or more threads want to access the same resource to work.
+As a result of them constantly waiting for each other to access the resource,
+system resources are negatively affected and even the application becomes unresponsive.
+ */
+
+public class DeadLockSolution2 {
+    public static void main(String[] args) {
+        String sugar = "sugar";
+        String coffee = "coffee";
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (coffee) {
+                    System.out.println(Thread.currentThread().getName() + " uses " + coffee + "...");
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                System.out.println(Thread.currentThread().getName() + " wants " + sugar);
+                synchronized (sugar) {
+                    System.out.println(Thread.currentThread().getName() + " uses " + sugar + "...");
+
+                }
+                System.out.println(Thread.currentThread().getName() + " added hot water and sipped his coffee.");
+            }
+        });
+        thread1.setName("Tom");
+        thread1.start();
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (coffee) {
+                    System.out.println(Thread.currentThread().getName() + " uses " + coffee + "...");
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                System.out.println(Thread.currentThread().getName() + " wants " + sugar);
+                synchronized (sugar) {
+                    System.out.println(Thread.currentThread().getName() + " uses " + sugar + "...");
+
+                }
+                System.out.println(Thread.currentThread().getName() + " added hot water and sipped his coffee.");
+
+            }
+        });
+        thread2.setName("Jerry");
+        thread2.start();
+
+
+    }
+}
